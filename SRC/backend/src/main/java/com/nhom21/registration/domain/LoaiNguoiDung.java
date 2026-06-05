7d415d2cfbaf97d9d43e0e1e816391f9ae018f64
datwhite21 <datnguyen2k521@gmail.com>
2026-06-06 01:46:20 +0700

    Delete SRC/backend directory

--- SRC/backend/src/main/java/com/nhom21/registration/domain/LoaiNguoiDung.java
+++
@@ -1,7 +0,0 @@
-package com.nhom21.registration.domain;
-
-public enum LoaiNguoiDung {
-    SINH_VIEN,
-    GIANG_VIEN,
-    GIAO_VU
-}
--- SRC/backend/src/main/java/com/nhom21/registration/domain/TrangThaiDangKy.java
+++
@@ -1,7 +0,0 @@
-package com.nhom21.registration.domain;
-
-public enum TrangThaiDangKy {
-    THANH_CONG,
-    DA_HUY,
-    CHO_DUYET
-}
